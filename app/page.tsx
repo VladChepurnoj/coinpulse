d5b4c88e7d0ce4dfac54e639b51f589ee10fec9c
Vlad Chepurnyi <chepurnojv@gmail.com>
2026-01-10 20:43:38 +0200

    implement coin overview and trending coins ui and functionality using the fetcher utility function

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import CoinOverview from "@/components/home/CoinOverview"
+import TrendingCoins from "@/components/home/TrendingCoins"
+import { Suspense } from "react"
 
-const Page = () => {
-  return (
-    <div></div>
+
+const Page = async () => {
+
+  return (<main
+    className="main-container"
+    >
+      <section className="home-grid">
+        <Suspense fallback={<div>Loading Overview...</div>}>
+          <CoinOverview />
+        </Suspense>
+
+        <Suspense fallback={<div>Loading Trending...</div>}>
+          <TrendingCoins />
+        </Suspense>
+      </section>
+
+      <section className="w-full mt-7 space-y-4">
+        <p>Categories</p>
+      </section>
+    </main>
   )
 }
 
