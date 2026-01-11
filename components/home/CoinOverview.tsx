c8d5c8b6aa7a3b34101e27813e87b5b9bc354724
Vlad Chepurnyi <chepurnojv@gmail.com>
2026-01-11 07:10:34 +0200

    implemented canddlestick chart

--- components/home/CoinOverview.tsx
+++ components/home/CoinOverview.tsx
@@ -1,14 +1,30 @@
 import { fetcher } from '@/lib/coingecko.actions'
 import Image from 'next/image'
+import CandlestickChart from '../CandlestickChart'
 
 const CoinOverview = async () => {
+    let coin
+    let coinOHLCData
 
-      const coin = await fetcher<CoinDetailsData>('/coins/bitcoin', {
+    try {
+      const [coin, coinOHLCData] = await Promise.all([
+        await fetcher<CoinDetailsData>('/coins/bitcoin', {
         dex_pair_format :"symbol"
-      })
-  return (
+      }),
+      await fetcher<OHLCData[]>('/coins/bitcoin/ohlc', {
+            vs_currency:'usd',
+            days:1,
+            invterval:'hourly',
+            precision:'full',
+          }),
+      ]);
+
+
+        return (
           <div id="coin-overview">
-          <div className="header pt-2">
+            <CandlestickChart data={coinOHLCData} coinId="bitcoin" >
+
+            <div className="header pt-2">
             <Image
             src={coin.image.large}
             alt={coin.name}
@@ -20,8 +36,19 @@ const CoinOverview = async () => {
               <h1>{coin.market_data.current_price.usd}</h1>
             </div>
           </div>
-        </div>
-  )
+            </CandlestickChart>
+            </div>
+        );
+
+    } catch (error) {
+      console.error("error fetching coin overview:", error)
+      return <CoinOverviewFallback />
+    }
+
+
+
+
+
 }
 
 export default CoinOverview
